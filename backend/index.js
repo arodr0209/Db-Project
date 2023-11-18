7663ddcf392b6b7467e841ec420a7df6e91ae01f
Rix95 <ricalrax@gmail.com>
2023-11-17 22:17:39 -0600

    add updatedelete

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import mysql from "mysql";
 import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -11,25 +13,24 @@ const db = mysql.createConnection({
   database: "test",
 });
 
-app.use(express.json());
-app.use(cors());
-
 app.get("/", (req, res) => {
-  res.json("hello this is the backend yoyo");
+  res.json("hello");
 });
 
 app.get("/books", (req, res) => {
-  db.query("SELECT * FROM books", (err, result) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
     if (err) {
       console.log(err);
-    } else {
-      res.json(result);
+      return res.json(err);
     }
+    return res.json(data);
   });
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
   const values = [
     req.body.title,
     req.body.desc,
@@ -38,14 +39,39 @@ app.post("/books", (req, res) => {
   ];
 
   db.query(q, [values], (err, data) => {
-    if (err) {
-      return res.json(err);
-    } else {
-      res.json("Book added!");
-    }
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
   });
 });
 
 app.listen(8800, () => {
-  console.log("Backend server is running!");
+  console.log("Connected to backend.");
 });
